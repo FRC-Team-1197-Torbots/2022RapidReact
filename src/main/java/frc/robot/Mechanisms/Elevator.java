302672757b6cf8767cf04c3522096dde751185fd
JustinTsai800 <charlesthefrank800@gmail.com>
2022-02-05 19:10:20 -0800

    added stub methods to the mechanism classes

---
+++ src/main/java/frc/robot/Mechanisms/Elevator.java
@@ -0,0 +1,20 @@
+package frc.robot.Mechanisms;
+
+public class Elevator {
+    public static enum elevatorState {
+        IDLE, RUN;
+    }
+
+    private elevatorState runElevator;
+
+    /*run(STATE)
+Switch-case
+States IDLE, RUN
+CASE IDLE
+Set elevator motor speed to 0
+CASE RUN
+Set elevator motor speed to x
+*/
+
+    
+}
---
+++ src/main/java/frc/robot/Mechanisms/Flywheel.java
@@ -0,0 +1,16 @@
+package frc.robot.Mechanisms;
+
+public class Flywheel {
+    public void init(){
+
+    }
+
+    public void revUp(double targetSpeed){
+
+    }
+
+    public void stop(){
+        
+    }
+    
+}
