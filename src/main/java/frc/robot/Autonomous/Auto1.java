ee9a0ca53a558b37975fbfdb258ee59a615c77dc
JustinTsai800 <charlesthefrank800@gmail.com>
2022-02-22 18:20:10 -0800

    Started to add some test code for the first autonomous path

---
+++ src/main/java/frc/robot/Autonomous/Auto1.java
@@ -0,0 +1,66 @@
+package frc.robot.Autonomous;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Autonomous.curveTrajectory.Direction;
+import frc.robot.Drive.TorDrive;
+import frc.robot.Mechanisms.*;
+
+public class Auto1 {
+    public static enum autoRun{
+        INIT, Linear1, Linear2, Curve1, DONE;
+    }
+
+    private autoRun AutoState1 = autoRun.INIT;
+
+    private MechMaster mechMaster;
+    private TorDrive torDrive;
+    private linearTrajectory linear1;
+    private linearTrajectory linear2;
+    private curveTrajectory curve1;
+    //private pivotTrajetory pivot1;
+    private double startTime;
+    private double currentTime;
+
+    private double currentPosition;
+    private double lastPosition;
+
+    private final double curve_outer_distance = (2f * Math.PI * 37.375) / 36f;
+
+    public Auto1(MechMaster mechMaster, TorDrive torDrive){
+        this.torDrive = torDrive;
+        this.mechMaster = mechMaster;
+        linear1 = new linearTrajectory(torDrive, 3.5, 3);
+        linear2 = new linearTrajectory(torDrive, 2.78613941667, 3);
+        curve1 = new curveTrajectory(torDrive, curve_outer_distance, 5, Direction.RIGHT);
+
+    }
+
+    public void run(){
+        currentTime = Timer.getFPGATimestamp();
+        switch(AutoState1){
+            case INIT:
+                linear1.init();
+                //keep the mechanics idle
+                AutoState1 = autoRun.Linear1;
+                break;
+            case Linear1:
+                linear1.run();
+                //run the intake
+                if(linear1.isDone()){
+                    curve1.init();
+                    AutoState1 = autoRun.Curve1;
+                }
+                break;
+            case Curve1:
+                curve1.run();
+                //shoot the ball
+                if(curve1.isDone()){
+                    linear2.init();
+                    AutoState1 = autoRun.Linear2;
+                }
+                break;
+            case Linear2:
+                
+        }
+    }
+}
